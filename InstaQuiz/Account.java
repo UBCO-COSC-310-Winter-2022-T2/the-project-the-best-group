928286243de6116dcd25d87c17a7c10653035ef2
gusgo <gusgo@host.docker.internal>
2023-03-26 19:32:24 -0700

    unit test 1.1, added createAccount method in Account.java

--- InstaQuiz/Account.java
+++ InstaQuiz/Account.java
@@ -1,5 +1,11 @@
 package InstaQuiz;
 
 public class Account {
+
+	public static boolean createAccount(String email, String password, String userType) {
+		
+		// TODO Auto-generated method stub
+		return false;
+	}
     
 }
--- InstaQuiz/Testing.java
+++ InstaQuiz/Testing.java
@@ -2,11 +2,32 @@ package InstaQuiz;
 
 import org.junit.Test;
 
-public class Testing 
-{
+public class Testing {
+    
+	
+	// Tests for functional requirement 1.1
     @Test
-    public void test1()
-    {
-        assert(true);
-    } 
+    public void testCreateStudentAccount() {
+    	
+        String email = "student@mail.com";
+        String password = "pw";
+        String userType = "student";
+
+        boolean createAccountResult = Account.createAccount(email, password, userType);
+        
+        assert(createAccountResult);
+    }
+    
+    @Test
+    public void testCreateInstructorAccount() {
+    	
+        String email = "instructor@mail.com";
+        String password = "pw";
+        String userType = "instructor";
+    
+        boolean createAccountResult = Account.createAccount(email, password, userType);
+
+        assert(createAccountResult);
+    }
+    // end of 1.1
 }
