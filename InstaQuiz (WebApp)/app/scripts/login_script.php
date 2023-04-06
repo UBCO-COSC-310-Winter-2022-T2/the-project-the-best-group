3a9982988c0726400293aa715ec7e8560a73af34
Patrick Ma <patrickma6199@gmail.com>
2023-04-05 19:15:13 -0700

    Refactored Login into Front and Back End Files

---
+++ InstaQuiz (WebApp)/app/scripts/login_script.php
@@ -0,0 +1,30 @@
+<?php
+    session_start();
+    require_once('../scripts/config.php');
+
+    if($_SERVER["REQUEST_METHOD"] == "POST") 
+    {
+        // Retrieve user's credentials from the POST request
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        // Retrieve user's information from the account table
+        $sql = "SELECT id, permission, password FROM accounts WHERE email = '$email'";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_assoc($result);
+
+        if (mysqli_num_rows($result) == 1 && password_verify($password, $row['password'])) {
+            $_SESSION['user_id'] = $row['id'];
+            $_SESSION['user_permission'] = $row['permission'];
+            header("Location: ../index.php");
+            exit;
+        } 
+        else 
+        {
+            // Display an error message if user's credentials are incorrect
+            echo '<div class="error-message">Invalid email or password.</div>';
+        }
+
+        mysqli_close($conn);
+    }
+?>
