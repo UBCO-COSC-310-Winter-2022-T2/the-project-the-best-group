1f38085cff4ea51169bd22367fa130dd845be825
Sten <112997109+Rev-rok@users.noreply.github.com>
2023-04-09 13:07:19 -0700

    script to get questions asyncronously for students in live session

---
+++ InstaQuiz (WebApp)/app/scripts/get_active_question.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function() {
+
+    getActiveQuestion();
+    //var interval = setInterval(getActiveQuestion, 5000);  //refresh comments every 5 seconds
+
+});
+
+function getActiveQuestion() {
+
+    // AJAX function to get new comments
+    var cid = location.search.replace("?cid=", "");
+    var xmlhttp= new XMLHttpRequest();
+    xmlhttp.onreadystatechange=function() {
+        if (this.readyState==4 && this.status==200) {
+            $('#question-container')[0].innerHTML = this.responseText;
+        }
+    }
+
+    xmlhttp.open("GET", "../scripts/get_active_question.php?id=" + cid, true);
+    xmlhttp.send();
+}
---
+++ InstaQuiz (WebApp)/app/scripts/get_active_question.php
@@ -0,0 +1,36 @@
+<?php
+
+    session_start();
+    if (!isset($_SESSION['user_id']) || !isset($_GET['id']))
+        exit();
+
+    require_once('config.php');
+
+    $cid = $_GET['id'];
+    $sql = "SELECT qid, prompt, a, b, c, d, answer FROM questions WHERE live=1 AND cid=".$cid;
+    $result = mysqli_query($conn, $sql);
+
+    if ($result->num_rows == 0) 
+        exit('<p>Welcome to class! Currently there are no active polls<p>');
+    
+
+    
+
+    while($row = $result->fetch_assoc()) {
+        $output .= "
+            <div class='question-item'>
+                <form action='../scripts/answer_question.php' method='POST'>
+                <h2>{$row['prompt']}</h2>
+                    <p><input type='radio' name='question{$row['qid']}' value='A' ><label>A) {$row['a']}</label></p>
+                    <p><input type='radio' name='question{$row['qid']}' value='B' ><label>B) {$row['b']}</label></p>
+                    <p><input type='radio' name='question{$row['qid']}' value='C' ><label>C) {$row['c']}</label></p>
+                    <p><input type='radio' name='question{$row['qid']}' value='D' ><label>D) {$row['d']}</label></p>
+                    <input type='hidden' name='qid' value='{$row['qid']}'>
+                    <button class='good-button' type='submit'>Submit Answer</button>
+                </form>
+            </div>";
+    }
+    mysqli_free_result($result);
+    mysqli_close($conn);
+    echo $output;
+?>
