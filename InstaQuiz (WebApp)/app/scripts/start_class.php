ed3cb38a5d2cc3da2627eb9b2bebca2bda1d50cb
Sten <112997109+Rev-rok@users.noreply.github.com>
2023-04-08 17:34:58 -0700

    add script for instructor to start class session

---
+++ InstaQuiz (WebApp)/app/scripts/start_class.php
@@ -0,0 +1,35 @@
+<?php
+    session_start();
+    
+    // logged in instructor made request
+    if ($_SESSION['user_permission'] != 1) {
+        echo "<script>alert('perm".$_SESSION['permission']."')</script>";
+        header('Location: ' . $_SERVER['HTTP_REFERER']);
+        exit();
+    }
+
+    // POST request
+    if ($_SERVER['REQUEST_METHOD'] != 'POST')  {
+        echo "<script>alert('post')</script>";
+        header('Location: ' . $_SERVER['HTTP_REFERER']);
+        exit();
+    }
+
+    require_once('config.php');
+    unset($_SESSION['result_message']);
+
+    $cid = $_POST['cid'];
+    $sql = "UPDATE courses SET live = 1 WHERE cid = ".$cid;
+
+    if (mysqli_query($conn, $sql))  {
+        mysqli_close($conn);
+        header('Location: ../session.php?cid='.$cid);
+        exit();
+    } else {
+        $_SESSION['result_message'] = "<div class='error-message'>Error starting class. Please try again</div>";
+        mysqli_close($conn);
+        header('Location: ../courses.php');
+        exit();
+    }
+        
+?>
