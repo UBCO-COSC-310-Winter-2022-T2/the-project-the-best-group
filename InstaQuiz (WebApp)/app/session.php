ce331b5d849d822876f90967bb40f69921863197
Sten <112997109+Rev-rok@users.noreply.github.com>
2023-04-09 13:03:55 -0700

    initial live session UI for instructor and student

--- InstaQuiz (WebApp)/app/session.php
+++ InstaQuiz (WebApp)/app/session.php
@@ -1,49 +1,21 @@
 <?php
     session_start();
-    
+    require_once('scripts/config.php');
+    $user_id = $_SESSION['user_id'];
+    $user_permission = $_SESSION['user_permission'];
+
     if (!isset($_GET['cid'])) {
         header('Location: ' . $_SERVER['HTTP_REFERER']);
         exit();
     }
     $cid = $_GET['cid'];
 
-    // TODO: get permission to load different instructor or student page
-
+    if($user_permission == "0")
+    {
+        header('Location: pages/session_student.php?cid='.$cid);
+    }
+    else if ($user_permission == "1")
+    {
+        header('Location: pages/session_instructor.php?cid='.$cid);
+    }
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>InstaQuiz</title>
-    <link rel="stylesheet" href="../css/body.css">
-    <style>
-        #end-container {
-            margin-top: 2em;
-            margin-left: auto;
-            margin-right: auto;
-            padding: 1em;
-            width: 80%;
-            height: auto;
-            background-color: #07223E;
-            border-radius: 15px;
-            border: 5px solid #061A2D;
-        }
-        #end-container button {
-            display: block;
-            margin-left: auto;
-            margin-right: auto;
-        }
-    </style>
-</head>
-<body>
-    <?php include_once('header.php'); ?>
-    <div id='end-container'>
-        <form action='scripts/end_class.php' method='POST'>
-            <?php echo "<input type='hidden' name='cid' value='{$cid}'>"; ?>
-            <button type='submit' class='bad-button'>End Class</button>
-        </form>
-    </div>
-
-
-</body>
-</html>
